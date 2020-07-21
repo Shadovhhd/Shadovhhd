3c6a84a2f90a0161476c0db634bb00e27921fc85
Тарас <52956067+Shadovhhd@users.noreply.github.com>
2020-07-21 11:39:23 +0300

    Add files via upload

---
+++ theyalow/js/main.js
@@ -0,0 +1,86 @@
+
+$(document).ready(function(){
+ 
+$('.aboutus-slick').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+   responsive: [
+
+
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        prevArrow: '<button class="prev"></button>',
+        nextArrow:'<button class="next"></button>',
+       
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+          prevArrow: '<button class="prev"></button>',
+        nextArrow:'<button class="next"></button>',
+      }
+    },
+    {
+      breakpoint: 582,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+          prevArrow: '<button class="prev"></button>',
+        nextArrow:'<button class="next"></button>',
+      }
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+          prevArrow: '<button class="prev"></button>',
+        nextArrow:'<button class="next"></button>',
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+			$('.button[filter="all"]').click(function(event) {
+				/* Act on the event */
+				$('.work__block > div').hide(300);
+				$('.work__block > div').show(300);
+
+			});
+			$('.button[filter="design"]').click(function(event) {
+				/* Act on the event */
+				$('.work__block > div').hide(300);
+				$('.work__block > div[filter="design"]').show(300);
+
+			});
+			$('.button[filter="branding"]').click(function(event) {
+				/* Act on the event */
+				$('.work__block > div').hide(300);
+				$('.work__block > div[filter="branding"]').show(300);
+
+			});
+			$('.button[filter="development"]').click(function(event) {
+				/* Act on the event */
+				$('.work__block > div').hide(300);
+				$('.work__block > div[filter="development"]').show(300);
+
+			});
+			$('.button[filter="strategy"]').click(function(event) {
+				/* Act on the event */
+				$('.work__block > div').hide(300);
+				$('.work__block > div[filter="strategy"]').show(300);
+
+			});
+		
+			
+	});
