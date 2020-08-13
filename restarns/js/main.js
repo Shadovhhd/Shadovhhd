94a7991d32451bbef83146d1443d5b3ad0612a47
Тарас <52956067+Shadovhhd@users.noreply.github.com>
2020-08-13 16:20:29 +0300

    Add files via upload

--- restarns/js/main.js
+++ restarns/js/main.js
@@ -16,18 +16,26 @@ $(document).ready(function () {
           },
         ]
       });  
-      $('.photo').slick({
-        infinite: true,
+      $('.more_content').slick({
+        infinite: false,
         speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToShow: 5,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 4,
               slidesToScroll: 1,
               infinite: true,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2,
+              infinite: true,
+
             }
           },
           {
@@ -36,6 +44,7 @@ $(document).ready(function () {
               slidesToShow: 1,
               slidesToScroll: 1,
               infinite: true,
+
             }
           },
         ]
