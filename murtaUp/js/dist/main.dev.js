1d19658c24a47ca071e420270c5a7e0861782414
Тарас <52956067+Shadovhhd@users.noreply.github.com>
2020-08-29 18:32:51 +0300

    Add files via upload

---
+++ murtaUp/js/dist/main.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function () {
+  $('.header-slider').slick({
+    arrows: false,
+    vertical: true,
+    dots: true,
+    dotsClass: 'header-dots' // autoplay: 2000,
+
+  });
+  $('.nav__btn').on('click', function () {
+    $('.nav__list').slideToggle();
+  });
+});
